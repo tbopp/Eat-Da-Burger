b0eaf19c80bb9543c1e3d17d8930700b398d85d3
byt3_h34d <mr.tybopp@gmail.com>
2018-09-12 15:44:27 -0500

    Creating DB, table data, and mySQL connection logic

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger Mary');
+INSERT INTO burgers (burger_name) VALUES ('MAGA Hero');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pit Boss', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Flying Pig', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Brie-z', true);
+INSERT INTO burgers (burger_name) VALUES ('Holy Shiitake');
---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Setting the port to be the default port on host OR 8080
+const PORT = process.env.PORT || 8080;
+
+// Defining an Express server variable
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Setup Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
