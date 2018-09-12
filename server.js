d35e5dc66804ea294f91da12a19aeb6c8b8bbde4
byt3_h34d <mr.tybopp@gmail.com>
2018-09-12 17:45:14 -0500

    Adding Handlebar views, burger handling logic, and controller access to server.js

--- server.js
+++ server.js
@@ -26,3 +26,14 @@ app.engine("handlebars", exphbs({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
