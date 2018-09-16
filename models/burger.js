ff909185b49a7901b0f1c2ec7e7ae5fbb511573b
byt3_h34d <mr.tybopp@gmail.com>
2018-09-16 00:20:09 -0500

    Updating formatting.

--- models/burger.js
+++ models/burger.js
@@ -18,11 +18,6 @@ let burger = {
             cb(res);
         });
     },
-    delete: function (condition, cb) {
-        orm.delete("burgers", condition, function (res) {
-            cb(res);
-        });
-    }
 };
 
 // Export the database functions for the controller (burgers_controller.js).
